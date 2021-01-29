499cabfa7ac9c99fafc7fcdf501568d8f918634e
AdrienM <privateadrieps@gmail.com>
2021-01-29 20:09:05 +0100

    Initial commit. List fragment vide.

---
+++ app/src/main/java/com/michelAdrien/AMTT0121/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.michelAdrien.AMTT0121
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_navigation_activity)
+
+
+
+        val toolbar: Toolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+
+
+        /*
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+        */
+
+    }
+
+    //Creation menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.burger_menu, menu)
+        Log.d("testAdrien", "nous avons inflate")
+        return true
+    }
+
+    //@TODO Put here switch to profile screen
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d("testAdrien", "clicksurlemenu")
+        return super.onOptionsItemSelected(item)
+    }
+}
